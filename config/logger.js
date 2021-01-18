3890f80122a6f58b4407e33e289ef2788e80da61
akhilgangula <akhilreddy.gangula96@gmail.com>
2021-01-17 19:01:27 -0500

    v0.0.3: added winston

--- config/logger.js
+++ config/logger.js
@@ -1,5 +1,5 @@
 import winston from 'winston';
-import {ENV}  from './config';
+import { ENV } from './config';
 
 const enumerateErrorFormat = winston.format((info) => {
     if (info instanceof Error) {
@@ -16,7 +16,7 @@ const logger = winston.createLogger({
             ? winston.format.colorize()
             : winston.format.uncolorize(),
         winston.format.splat(),
-        winston.format.printf(({ level, message }) => `${level}: ${message}`)
+        winston.format.printf(({ level, message }) => `${level}: ${message}`),
     ),
     transports: [
         new winston.transports.Console({
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,16 +1,12 @@
 const config = require('../config/config');
 
-const constructResetUrl = (token) => {
-    return config.env === 'development'
-        ? `https://localhost:8081/reset/${token}`
-        : `https://auth.timetotrain.fit/reset/${token}`;
-};
+const constructResetUrl = (token) => (config.env === 'development'
+    ? `https://localhost:8081/reset/${token}`
+    : `https://auth.timetotrain.fit/reset/${token}`);
 
-const constructActivateUrl = (token) => {
-    return config.env === 'development'
-        ? `https://localhost:8081/activate/${token}`
-        : `https://auth.timetotrain.fit/activate/${token}`;
-};
+const constructActivateUrl = (token) => (config.env === 'development'
+    ? `https://localhost:8081/activate/${token}`
+    : `https://auth.timetotrain.fit/activate/${token}`);
 
 module.exports = {
     constructResetUrl,
